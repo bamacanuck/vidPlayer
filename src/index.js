01e29ddacc6ab93a805ee112a3068e3865b9f851
Shane Scott <shaneradio@gmail.com>
2018-07-11 18:09:55 -0400

    new component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@
 
 // create a new component - which produces some HTML
 
+const App = function() {
+    return <div>hello hello, good people</div>
+}
+
 // take component-generated HTML, and display it
 // on the page(in the DOM)
