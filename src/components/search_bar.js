e0333a25d5713edfe4c70a2114d9f789337fb521
Shane Scott <shaneradio@gmail.com>
2018-07-13 14:58:23 -0400

    changing state with input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,12 +13,16 @@ constructor(props) {
 }
 
 	render() {
-		return <input onChange={this.onInputChange} />;
+		return <input onChange={event => this.setState({ term: event.target.value })} />;
 	}
 
-	onInputChange(event) {
-		console.log(event.target.value);
-	}
+	// render() {
+	// 	return <input onChange={this.onInputChange} />;
+	// }
+
+	// onInputChange(event) {
+	// 	console.log(event.target.value);
+	// }
 }
 
 export default SearchBar;
