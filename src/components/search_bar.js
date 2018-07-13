853192f5cf1bad4d8bdf2e85e43c950660154369
Shane Scott <shaneradio@gmail.com>
2018-07-13 12:34:45 -0400

    search bar function under construction

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,7 +6,11 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	render() {
-		return <input />;
+		return <input onChange={this.onInputChange} />;
+	}
+
+	onInputChange() {
+
 	}
 }
 
