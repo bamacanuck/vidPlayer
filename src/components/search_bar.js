d3b98e6e4991e12fcc07701f0ebbaa8cfba8c930
Shane Scott <shaneradio@gmail.com>
2018-07-13 14:41:01 -0400

    getting into state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,6 +5,13 @@ import React, { Component } from 'react';
 // import myKeys from './myKeys';
 
 class SearchBar extends Component {
+
+constructor(props) {
+	super(props);
+
+	this.state = { term: '' };
+}
+
 	render() {
 		return <input onChange={this.onInputChange} />;
 	}
