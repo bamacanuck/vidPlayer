fc0edb26c802bcf146a222a5631d1664b1838170
Shane Scott <shaneradio@gmail.com>
2018-07-17 17:51:30 -0400

    importing vidList component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import ourKeys from './ourKeys';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 const API_KEY = ourKeys.YT_API_KEY;
 
 
