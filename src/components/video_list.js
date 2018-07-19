5bdc54bb2bcf233df13fffb26371293378d225c6
Shane Scott <shaneradio@gmail.com>
2018-07-19 17:34:55 -0400

    further building video_list.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,6 +3,10 @@ import VideoListItem from './video_list_item';
 
 
 const VideoList = (props) => {
+
+    props.videos.map((video) => {
+			return <
+    });
     
     return (
         <ul className="col-md-4 list-group">
