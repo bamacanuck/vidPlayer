57446af63bc12afbf859e538c7c51ff01586220f
Shane Scott <shaneradio@gmail.com>
2018-07-19 18:13:14 -0400

    building in thumbnails from returned data

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,11 +3,17 @@ import React from 'react';
 const VideoListItem = (props) => {
 	const video = props.video;
 
+	const imageURL = video.snippet.thumbnails.default.url;
+
   return (
     <li className="list-group-item">
 			<div className="video-list media">
 				<div className="media-left">
 					<img className="media-object" />
+				</div>
+				
+				<div className="media-body">
+					<div className="media-heading"></div>
 				</div>
 			</div>
 		</li>
