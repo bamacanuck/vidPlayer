ab31e0d694c72e7464f7d8485b4947518d6a459e
Shane Scott <shaneradio@gmail.com>
2018-07-24 15:32:22 -0400

    refining VideoDetail

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const VideoDetail = (props) => {
-    return (
-        <div className="video-detail col-md-8">
-            <div className="embed-responsive embed-responsive-16by9">
-							<iframe className="embed-responsive-item"></iframe>
-            </div>
-						<div className="details">
-							<div>placeholder-title</div>
-							<div>placeholder-description</div>
-						</div>
+const VideoDetail = ({video}) => {
+	
+  return (
+    <div className="video-detail col-md-8">
+        <div className="embed-responsive embed-responsive-16by9">
+					<iframe className="embed-responsive-item"></iframe>
         </div>
-    )
+				<div className="details">
+					<div>{video.snippet.title}</div>
+					<div>{video.snippet.description}</div>
+				</div>
+    </div>
+  )
 
 };
 
