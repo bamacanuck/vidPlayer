5aec5de0d9db0855f81fcc4ea82df8e09d957dad
Shane Scott <shaneradio@gmail.com>
2018-07-25 15:39:05 -0400

    refactoring search elements in search_bar.js

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -17,10 +17,14 @@ constructor(props) {
 			<div className="search-bar">
 				<input
 				value={this.state.term}
-				onChange={event => this.setState({ term: event.target.value })} 
+				onChange={event => this.onInputChange(event.target.value )} 
 				/>
 			</div>
 		);
+	}
+
+	onInputChange (term) {
+		
 	}
 
 	// render() {
