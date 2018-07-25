3a390bdf06a4e66379265733d42257c5d990f449
Shane Scott <shaneradio@gmail.com>
2018-07-25 13:05:45 -0400

    console check for selected video

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ class App extends Component {
         videos: videos,
         selectedVideo: videos[0]
       });
-      // console.log(videos);
+      // console.log(this.state.selectedVideo);
     });
 
   }
